cb3f067c93d210c1468e3bb2652e490b3adfeb53
zeyad ayman elmaghrabi <161942240+ziadelmaghrabi@users.noreply.github.com>
2026-03-01 16:52:40 +0200

    max Number

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
-#include <iostream>
+ #include <iostream>
 using namespace std;
 
-void PrintString(int num)
+int maxnum(int arr[], int index)
 {
-    if(num==0)
-        return;
-    cout<<"I LOVE RECERUSION"<<endl;
-    PrintString(num-1);
+    if(index == 0)  // Base case
+        return arr[0];
+
+    int localMax = maxnum(arr, index - 1);
+
+    if(arr[index] > localMax)
+        return arr[index];
+    else
+        return localMax;
 }
 
 int main()
 {
-    PrintString(3);
+    int arr[100];
+    int n;
 
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+ cout<<maxnum(arr,n-1);
     return 0;
 }
